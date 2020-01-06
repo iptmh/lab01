12b2acec232c218401636270946156345240f530
iptmh <tammyip@gmail.com>
2020-01-06 08:14:43 -0800

    update

--- index.js
+++ index.js
@@ -4,4 +4,23 @@ const validator = require('./lib/validator.js');
 
 validator.isValid();
 
-console.log('apple', validator.isString('input'))
+const personRules = {
+  fields: {
+    id: { type: 'string', required: true },
+    name: { type: 'string', required: true },
+    age: { type: 'number', required: true },
+    children: { type: 'array', valueType: 'string' },
+  },
+};
+
+console.log('apple', validator.isString('personRules'))
+
+const susan = {
+  id: '123-45-6789',
+  name: 'Susan McDeveloperson',
+  age: 37,
+  children: [],
+};
+
+console.log('orange', validator.isString('susan'))
+
